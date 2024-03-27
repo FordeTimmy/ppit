d9b9d5bfecf2242078a90406240569129242be0f
Timmy Forde <you@example.com>
2024-03-27 22:14:51 +0000

    Got the products page displaying images

--- src/components/ElectricBikeList.js
+++ src/components/ElectricBikeList.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import bikesData from './bikesData';
+import './ElectricBikeList.css'; // Correct import statement for CSS file
 
 const ElectricBikeList = () => {
   return (
     <div>
       <h1>Electric Bikes</h1>
-      <ul>
-        {bikesData.map(bike => (
-          <li key={bike.id}>
-            {/* Link to individual bike details */}
-            <Link to={`/Electricbikes/${bike.id}`}>{bike.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="bikes-container">
+        <div className="row">
+          {bikesData.slice(0, 3).map(bike => (
+            <div key={bike.id} className="bike-item">
+              <Link to={`/Electricbikes/${bike.id}`}>
+                <img src={Array.isArray(bike.images) && bike.images.length > 0 ? bike.images[0] : bike.image} alt={bike.name} className="bike-image" />
+                <div className="bike-info">
+                  <h2>{bike.name}</h2>
+                  <p>{bike.price}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
