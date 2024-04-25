ca9b724fe343800a165b6d2e8d23fbc6d0e922a4
Conor Roberts <conorrroberts@gmail.com>
2024-04-25 22:20:17 +0100

    deleted unncessary things

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import Navbar from './components/Navbar';
 import CartPage from './components/CartPage';
 import Accessories from './components/Accessories';
 import ElectricBikeList from './components/ElectricBikeList';
-import BikeDetails from './components/BikeDetails';
+
 import KidsBikeList from './components/KidsBikeList';
-import KidsBikeDetails from './components/KidsBikeDetails';
+
 import MountainBikeList from './components/MountainBikeList';
 import WomensBikeList from './components/WomensBikeList';
-import WomensBikeDetails from './components/WomensBikeDetails';
+
 import Footer from './components/Footer';
 import BikeLocksList from './components/BikeLocksList';
 import BikeLightsList from './components/BikeLightsList';
-import BikeLightsDetails from './components/BikeLightsDetails';
+
 import BikeReflectorsList from './components/BikeReflectorsList';
 import CheckoutPage from './components/CheckoutPage';
 import Bikes from './components/Bikes';
@@ -52,15 +52,15 @@ const App = () => {
             <Route path="/admin" element={<AdminLogin handleAdminLogin={handleAdminLogin} />} />
             <Route path="/admin-dashboard" element={isAdmin ? <AdminDashboard isAdmin={isAdmin} /> : <Navigate to="/home" />} />
             <Route path="/electricbikes" element={<ElectricBikeList />} />
-            <Route path="/electricbikes/:id" element={<BikeDetails />} />
+            
             <Route path="/kidsbikes" element={<KidsBikeList />} />
-            <Route path="/kidsbikes/:id" element={<KidsBikeDetails />} />
+
             <Route path="/accessories" element={<Accessories />} />
             <Route path="/mountainbikes" element={<MountainBikeList />} />
             <Route path="/womensbikes" element={<WomensBikeList />} />
-            <Route path="/womensbikes/:id" element={<WomensBikeDetails />} />
+     
             <Route path="/bikelights" element={<BikeLightsList />} />
-            <Route path="/bikelights/:id" element={<BikeLightsDetails />} />
+       
             <Route path="/bikelocks" element={<BikeLocksList />} />
             <Route path="/bikereflectors" element={<BikeReflectorsList />} />
             <Route path="/cart" element={<CartPage />} />
